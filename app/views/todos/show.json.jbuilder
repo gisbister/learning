b0efe3af63e11e8ba1cd9474c8f1ae3b9c5c1bd3
Graeme Isbister <graeme@isbister.me>
2013-08-02 22:35:32 -0400

    Created

---
+++ app/views/todos/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @todo, :name, :due_on, :completed, :created_at, :updated_at
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Treebook::Application.config.secret_key_base = 'f30029407a29039444dd5a9643b6536ef0acf436d6c41b2007835f897ee1ed44a9d98fb308ca41eb4de64a165775eec450768f4b2149b3d96626001479d7351f'
